6d608384e69b091f564b4ee652cfbd2b8dab80f6
andresbur10 <andresbur10@hotmail.com>
2025-08-27 00:42:22 -0500

    refactor(ui): mostrar nombres de equipos y jugadores en standings y goleadores

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'domain/models.dart';
+import 'domain/standings.dart';
+import 'domain/stats.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +10,87 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    // Datos de ejemplo
+    final teamA = Team(id: 'A', name: 'Alpha');
+    final teamB = Team(id: 'B', name: 'Beta');
+    final playerA = Player(id: 'P1', teamId: 'A', name: 'Andrés');
+    final playerB = Player(id: 'P2', teamId: 'B', name: 'Bruno');
+
+    final match = Match(
+      id: 'M1',
+      tournamentId: 'T1',
+      homeTeamId: 'A',
+      awayTeamId: 'B',
+      events: [
+        MatchEvent(
+          id: 'E1',
+          matchId: 'M1',
+          teamId: 'A',
+          playerId: 'P1',
+          minute: 5,
+          type: EventType.goal,
+        ),
+        MatchEvent(
+          id: 'E2',
+          matchId: 'M1',
+          teamId: 'B',
+          playerId: 'P2',
+          minute: 25,
+          type: EventType.goal,
+        ),
+        MatchEvent(
+          id: 'E3',
+          matchId: 'M1',
+          teamId: 'A',
+          playerId: 'P1',
+          minute: 80,
+          type: EventType.goal,
+        ),
+      ],
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+    final tournament = Tournament(
+      id: 'T1',
+      name: 'Liga de Prueba',
+      teams: [teamA, teamB],
+      players: [playerA, playerB],
+      matches: [match],
+    );
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    final standings = computeStandings(tournament);
+    final scorers = computeTopScorers(tournament);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    // Mapas para lookup rápido
+    final teamsById = {for (var t in tournament.teams) t.id: t};
+    final playersById = {for (var p in tournament.players) p.id: p};
 
-  final String title;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Gestor de Torneos')),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: ListView(
+            children: [
+              const Text(
+                'Tabla de posiciones',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              // Standings
+              for (final row in standings)
+                Text(
+                  '${teamsById[row.teamId]!.name}: '
+                  '${row.points} pts (GF: ${row.gf}, GA: ${row.ga})',
+                ),
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+              // Goleadores
+              for (final s in scorers)
+                Text('${playersById[s.playerId]!.name} - ${s.goals} goles'),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
